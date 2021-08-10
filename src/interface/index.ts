3e425b99d11c2b800db108d71826d76dfd1e8267
winsonsunodm9n <gaozhenfa30972@126.com>
2021-08-10 13:23:11 +0800

    update: 补充 IPSData 接口

--- src/interface/index.ts
+++ src/interface/index.ts
@@ -1,3 +1,5 @@
 export { GlobalContextData } from './global-context-data/global-context-data';
 export { WSMessage } from './ws-message/ws-message';
 export { CliAPIData } from './cli-api-data/cli-api-data';
+// 默认标准请求回来的数据
+export type IPSData = Record<string, any>;
