16582dab24b5a080464cd9257b8d014fa217cc08
winsonsunodm9n <gaozhenfa30972@126.com>
2021-08-19 12:17:21 +0800

    add: 获取系统运行任务接口

--- src/service/service-api/service-api.service.ts
+++ src/service/service-api/service-api.service.ts
@@ -1,8 +1,9 @@
 import { ctx } from '../../context';
+import { IPSData } from '../../interface';
 import { Fetch, showErrInfo } from '../../util';
 
 /**
- *
+ * MosDynamic 服务
  *
  * @author chitanda
  * @date 2021-11-27 02:11:31
@@ -10,9 +11,9 @@ import { Fetch, showErrInfo } from '../../util';
  * @class ServiceApiService
  */
 export class ServiceApiService {
-  async getAppData(): Promise<unknown> {
+  async getAppData(): Promise<IPSData> {
     try {
-      const res = await Fetch.get(`${ctx.studioAddress}/MosDynamic/appdata`, { psdevslnsys: ctx.get('psdevslnsys') }, { headers: { 'Content-Type': 'application/json' } });
+      const res = await Fetch.get(`${ctx.studioAddress}/MosDynamic/appdata`, { psdevslnsys: ctx.get('psdevslnsys') }, { headers: { 'content-type': 'application/json' } });
       if (res?.data) {
         return res.data;
       }
@@ -20,6 +21,23 @@ export class ServiceApiService {
       showErrInfo(err);
     }
     return {};
+  }
+
+  async getSystemRun(): Promise<IPSData[]> {
+    try {
+      const psDevSlnSys: string = ctx.get('psdevslnsys')!;
+      const res = await Fetch.get(
+        `${ctx.studioAddress}/MosDynamic/pssysdevbktasks/fetchdefault`,
+        { psdevslnsysid: psDevSlnSys, n_taskstate_in: '10,20', size: 100, page: 0 },
+        { headers: { 'content-type': 'application/json', psdevslnsys: psDevSlnSys } },
+      );
+      if (res?.data) {
+        return res.data;
+      }
+    } catch (err) {
+      showErrInfo(err);
+    }
+    return [];
   }
 }
 
