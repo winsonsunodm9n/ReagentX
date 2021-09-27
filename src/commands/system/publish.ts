fd82be621b90105543cda639e70c91418cdfc499
winsonsunodm9n <gaozhenfa30972@126.com>
2021-09-27 09:30:41 +0800

    change: 建立代码发布任务，pick 选择模式重写

--- src/commands/system/publish.ts
+++ src/commands/system/publish.ts
@@ -1,10 +1,10 @@
-import { commands, window } from 'vscode';
+import { commands, Uri, window } from 'vscode';
 import { CommandConst, RTContextConst } from '../../constants';
 import { ctx } from '../../context';
 import { SystemRunPickItem } from '../../entities';
 import { CoreAPI } from '../../service';
 import { taskBar } from '../../status-bar';
-import { getErrMessage } from '../../util';
+import { FileUtil } from '../../util';
 
 /**
  * 系统发布
@@ -21,29 +21,46 @@ export class SystemPublishCommand {
 
   protected async execute(): Promise<void> {
     const psDevSlnSys = ctx.get('psdevslnsys');
-    // 加载可刷新的模板
-    const promise = new Promise<readonly SystemRunPickItem[]>(async resolve => {
-      const arr: SystemRunPickItem[] = [];
-      const sysRuns = await CoreAPI.curSystemRuns();
-      sysRuns.forEach(item => {
-        arr.push(new SystemRunPickItem(item));
-      });
-      resolve(arr);
-    });
-    // 选择模板
-    const sysRun = await window.showQuickPick(promise, {
-      title: '代码发布',
-      placeHolder: '请选择需要运行的发布模式',
-    });
-    if (!sysRun) {
-      return;
+    const pick = window.createQuickPick<SystemRunPickItem>();
+    pick.title = '代码发布';
+    pick.placeholder = '请选择需要运行的发布模式';
+    const btnTooltip = '建立发布配置';
+    pick.buttons = [
+      {
+        tooltip: btnTooltip,
+        iconPath: {
+          dark: FileUtil.resources.resolveResourceUri('icon/dark/add.svg'),
+          light: FileUtil.resources.resolveResourceUri('icon/light/add.svg'),
+        },
+      },
+    ];
+    pick.busy = true;
+    pick.show();
+    const sysRuns = await CoreAPI.curSystemRuns();
+    const arr: SystemRunPickItem[] = sysRuns.map(item => new SystemRunPickItem(item));
+    if (arr.length === 0) {
+      pick.placeholder = '暂无发布配置，请点击输入框右上方的添加按钮建立';
     }
-    ctx.setContext(RTContextConst.PUBLISH_CODE, true);
-    taskBar.warn({ text: `$(loading~spin) 正在建立发布任务` });
-    const res = await CoreAPI.cli('ExecuteSysCLICmd', { pstscmdname: 'devsys_pubcode', psdevslnsysid: psDevSlnSys, data: { sysrun: sysRun.data.pssystemrunname } });
-    if (res && res.status !== 200) {
-      taskBar.hide();
-      ctx.setContext(RTContextConst.PUBLISH_CODE, false);
-    }
+    pick.items = arr;
+    pick.busy = false;
+    pick.onDidChangeSelection(async items => {
+      if (items.length > 0) {
+        const run = items[0];
+        ctx.setContext(RTContextConst.PUBLISH_CODE, true);
+        taskBar.warn({ text: `$(loading~spin) 正在建立发布任务` });
+        const res = await CoreAPI.cli('ExecuteSysCLICmd', { pstscmdname: 'devsys_pubcode', psdevslnsysid: psDevSlnSys, data: { sysrun: run.data.pssystemrunname } });
+        if (res && res.status !== 200) {
+          taskBar.hide();
+          ctx.setContext(RTContextConst.PUBLISH_CODE, false);
+        }
+      }
+      pick.hide();
+    });
+    pick.onDidTriggerButton(e => {
+      if (e.tooltip === btnTooltip) {
+        const uri = Uri.parse(`${ctx.toolAddress}/mos-dynamic-mgr/#/mosdynamicmgr/${ctx.get('psdevslnsys')}/pssystemruns/views/cursysgridview`);
+        commands.executeCommand(CommandConst.VSCODE.OPEN, uri);
+      }
+    });
   }
 }
